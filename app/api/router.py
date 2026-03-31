1c6d6a00df7efe5e3a2237e9eb3e8df760b2f6b1
Yannaev <69728833+yannaev@users.noreply.github.com>
2026-03-31 17:06:18 +0500

    edit imports

--- app/api/router.py
+++ app/api/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.auth import router as auth_router
+from app.api import auth
 
 main_router = APIRouter()
 
-main_router.include_router(auth_router)
+main_router.include_router(auth.router)
